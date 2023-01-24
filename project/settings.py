1b08f318aa8381025952fe8457ed09dcdeb91fcc
S.Miah <serhanmiah@icloud.com>
2023-01-24 19:22:56 +0000

    env file updated

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# load dotenv
+# load dotenv 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
